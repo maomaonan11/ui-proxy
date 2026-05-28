864e47e0a518b90f4503bdc1f7d3b3176ac3bd4f
maomaonan11 <84073163+maomaonan11@users.noreply.github.com>
2026-05-28 17:40:29 +0800

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -9,7 +9,6 @@ exports.handler = async (event) => {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Referer": new URL(targetUrl).origin,
       },
-      ignoreURI: true,
       credentials: "omit"
     });
     const body = await response.text();
